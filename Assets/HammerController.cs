0e157ae7a8f3db8d335a228ac48eb39d61f3de77
家子智成 <>
2020-08-26 00:42:05 +0900

    ハンマー作成

---
+++ Assets/HammerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class HammerController : MonoBehaviour
+{
+    [SerializeField] bool isStartLeft;
+    void Start()
+    {
+        float dir = isStartLeft ? 1 : -1;
+        Sequence sequence = DOTween.Sequence()
+        .Append(transform.DORotate(Vector3.right * dir * 180, 5).SetRelative().SetEase(Ease.InOutQuart))
+        .Append(transform.DORotate(-Vector3.right * dir * 180, 5f).SetRelative().SetEase(Ease.InOutQuart));
+        sequence.SetLoops(-1);
+    }
+
+
+}

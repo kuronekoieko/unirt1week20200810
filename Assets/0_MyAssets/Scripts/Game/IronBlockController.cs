55905331edb4558e5918bab2eff02d6d5d62254f
家子智成 <>
2020-08-26 00:04:21 +0900

    鉄ブロック調整

--- Assets/0_MyAssets/Scripts/Game/IronBlockController.cs
+++ Assets/0_MyAssets/Scripts/Game/IronBlockController.cs
@@ -5,10 +5,11 @@ using DG.Tweening;
 
 public class IronBlockController : MonoBehaviour
 {
-
+    [SerializeField] float startDelay;
     void Start()
     {
         Sequence sequence = DOTween.Sequence()
+        .AppendInterval(startDelay)
         .Append(transform.DOMoveY(20, 5).SetRelative().SetEase(Ease.Linear))
         .Append(transform.DOMoveY(-20, 0.5f).SetRelative().SetEase(Ease.Linear))
         .AppendInterval(1);

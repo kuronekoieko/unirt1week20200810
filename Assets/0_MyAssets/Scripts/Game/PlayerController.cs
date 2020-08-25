19ecd5db209ab2332c63afde042ba359cf47b9ee
家子智成 <>
2020-08-25 22:28:18 +0900

    回転動作を仮実装

---
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform center;
+    Rigidbody[] rigidbodies;
+
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+
+        for (int i = 0; i < rigidbodies.Length; i++)
+        {
+            var vec = Vector3.Cross(rigidbodies[i].transform.position - center.position, Vector3.forward);
+            if (vec.y > 0) continue;
+            if (vec.x < 0) continue;
+            rigidbodies[i].velocity = vec * 10;
+        }
+    }
+}

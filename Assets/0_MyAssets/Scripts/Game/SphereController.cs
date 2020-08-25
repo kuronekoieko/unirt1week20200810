7f848e3b3858e6b556a1ece71a0b34074f2646ad
家子智成 <>
2020-08-26 00:24:34 +0900

    くす玉トラップ

---
+++ Assets/0_MyAssets/Scripts/Game/SphereController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float speed;
+    Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10f)
+        {
+            transform.position = startPos;
+        }
+        var vel = rb.velocity;
+        vel.z = -speed;
+        rb.velocity = vel;
+    }
+}
